893cdce3c47026bd340a97de02748303a67fa871
Asifur Rahaman <asifurrahamanofficial@gmail.com>
2022-08-05 10:20:21 +0600

    a sass landing page added

---
+++ 103-sass landing-page/script.js
@@ -0,0 +1,16 @@
+// carousel
+let article_carousel_items = document.querySelectorAll('.article-carousel .carousel-item')
+
+article_carousel_items.forEach((el) => {
+    const minPerSlide = 3
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+        	next = article_carousel_items[0]
+      	}
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
